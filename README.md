853c429bbdea9c1a43ba04556f600caca4c0b567
Tom Black <blacktm@gmail.com>
2016-07-26 21:59:42 -0400

    Add `brew install --HEAD` option

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Install this tap using:
 brew tap simple2d/tap
 brew install simple2d
 ```
+
+Or, install the latest development release using:
+
+```bash
+brew install --HEAD simple2d
+```
--- simple2d.rb
+++ simple2d.rb
@@ -4,6 +4,8 @@ class Simple2d < Formula
   url "https://github.com/simple2d/simple2d/archive/v0.3.0.tar.gz"
   sha256 "a39955508c14d278e812b447ca4fed46ad3e3c5a36d5e0654a01eff878a171e3"
 
+  head "https://github.com/simple2d/simple2d.git"
+
   depends_on "sdl2"
   depends_on "sdl2_image"
   depends_on "sdl2_mixer" => %w[with-flac with-smpeg2 with-libvorbis]
