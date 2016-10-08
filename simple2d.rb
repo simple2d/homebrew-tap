7a708be71b529100495f6bacbae5a386215c8235
Tom Black <blacktm@gmail.com>
2016-10-08 16:05:30 -0400

    Bump version to 0.4.1

--- simple2d.rb
+++ simple2d.rb
@@ -1,8 +1,8 @@
 class Simple2d < Formula
   desc "Simple, open-source 2D graphics for everyone"
   homepage "http://simple2d.com"
-  url "https://github.com/simple2d/simple2d/archive/v0.4.0.tar.gz"
-  sha256 "951847a8b71728a3843bd96085c2cb1503bac54b312bd2a936f8ede1d2c4197a"
+  url "https://github.com/simple2d/simple2d/archive/v0.4.1.tar.gz"
+  sha256 "14887ba5122da44964572c30c15f45ae53ed7da37291f86a8744c4086ed90f30"
 
   head "https://github.com/simple2d/simple2d.git"
 
