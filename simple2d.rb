d8842be5d9a7185f91c7f5ec60100a840796317e
Tom Black <blacktm@gmail.com>
2017-09-18 22:58:15 -0700

    Bump version to 0.7.0, add Apple frameworks

--- simple2d.rb
+++ simple2d.rb
@@ -1,19 +1,25 @@
 class Simple2d < Formula
   desc "Simple, open-source 2D graphics for everyone"
   homepage "http://simple2d.com"
-  url "https://github.com/simple2d/simple2d/archive/v0.6.0.tar.gz"
-  sha256 "41000df1d0261fe02507aa1a747d9674a07b835008d690c58299702dd1ef6711"
+  url "https://github.com/simple2d/simple2d/archive/v0.7.0.tar.gz"
+  sha256 "719a980efb4cc2dee9398b3100fbf00d0dabc339ac393124fe9ff9a2d5a2676a"
 
   head "https://github.com/simple2d/simple2d.git"
 
   depends_on "sdl2"
   depends_on "sdl2_image"
-  depends_on "sdl2_mixer" => %w[with-flac with-smpeg2 with-libvorbis]
+  depends_on "sdl2_mixer" => %w[with-flac with-smpeg2]
   depends_on "sdl2_ttf"
+
+  resource "frameworks" do
+    url "https://github.com/simple2d/simple2d/releases/download/v0.7.0/simple2d-apple-frameworks-0.7.0.zip"
+    sha256 "c33347a2fd115efc8982fdd1891b473ff5cb0f2eeca1e78a011879a697da7086"
+  end
 
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
+    frameworks.install resource("frameworks")
   end
 
   test do
