a3f896da789289ad1695d39e0574430984c0e592
Tom Black <blacktm@gmail.com>
2018-01-29 21:39:17 -0800

    Bump version to 0.9.0

--- simple2d.rb
+++ simple2d.rb
@@ -1,8 +1,8 @@
 class Simple2d < Formula
   desc "Simple, open-source 2D graphics for everyone"
   homepage "http://simple2d.com"
-  url "https://github.com/simple2d/simple2d/archive/v0.8.0.tar.gz"
-  sha256 "f41760494549c45945c39c5d0f31e53c3c68f849827db1fa9832dbb5a2b1f61f"
+  url "https://github.com/simple2d/simple2d/archive/v0.9.0.tar.gz"
+  sha256 "c03497b47a74dd220be8ff3aeedfa42bce36babd23df582281bf0f0c8b48ef0f"
 
   head "https://github.com/simple2d/simple2d.git"
 
@@ -13,8 +13,8 @@ class Simple2d < Formula
   depends_on "sdl2_ttf"
 
   resource "frameworks" do
-    url "https://github.com/simple2d/simple2d/releases/download/v0.8.0/simple2d-apple-frameworks-0.8.0.zip"
-    sha256 "bd1bbf077626c8fb67449f2e66dddcfc0ad433f29f71ac3a5454b03a1dbdff3e"
+    url "https://github.com/simple2d/simple2d/releases/download/v0.9.0/simple2d-apple-frameworks-0.9.0.zip"
+    sha256 "9b270d3c1fc5baa490e15087c6fc431ce97872264dcf503ae005092124858b2f"
   end
 
   def install
